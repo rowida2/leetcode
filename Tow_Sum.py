c72a2a248f3e3f34ee0772776c6e2fd239c6f165
rowida2 <rowaidasamer22@gmail.com>
2025-08-04 02:30:44 +0300

    Update Tow_Sum.py

--- Tow_Sum.py
+++ Tow_Sum.py
@@ -1,15 +1,8 @@
 class Solutions:
     def towsum(self , nums , target):
 
-        i=0
-        y=target-nums[i]
-        for i in nums:
-            if nums[i]==y:
-                return [i, i+1]
-            else:
-                i=i+1
-
-s=Solutions()
-nums=[2,7,11,15]
-target=9
-print(s.towsum(nums, target))               
+       for i in range(len(nums)):
+           for j in range(i+1, len(nums)):
+               if nums[j]==target-nums[i]:
+                   return [i, j]
+       return []
