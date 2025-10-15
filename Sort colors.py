262098b9ed8c740615738e4e93dac4e7861e7efc
rowida2 <rowaidasamer22@gmail.com>
2025-10-15 17:58:53 +0300

    Create Sort colors.py
    https://leetcode.com/problems/sort-colors/description/

---
+++ Sort colors.py
@@ -0,0 +1,21 @@
+# Solution Using Two Pointers
+# Medium
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, i, right = 0 , 0, len(nums)-1
+        
+        while i <= right:
+
+            if nums[i]== 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left +=1
+                i +=1
+            elif nums[i] == 2 :
+                nums[right], nums[i] = nums[i], nums[right]
+                right -=1
+            else:
+                i +=1
+           
