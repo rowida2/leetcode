9bfb5af282adca88f39e997ab4c7014b3e9330d4
rowida2 <rowaidasamer22@gmail.com>
2025-12-12 04:13:50 +0200

    implement getConcatenation method to concatenate array

---
+++ Array/Concatenation of Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+      n = length(nums)
+      ans = empty list
+
+      for i in range(n):
+        ans.append(nums[i])
+
+      for i in range(n):
+        ans.append(nums[i])
+
+     return ans
